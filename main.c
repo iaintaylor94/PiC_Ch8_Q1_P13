dbfd855bb8611b2c5f47578487602f6f79b19672
Iain Taylor <iain.taylor.94@gmail.com>
2023-07-24 12:34:27 +0000

    copied program

--- main.c
+++ main.c
@@ -1,6 +1,53 @@
 #include <stdio.h>
 
+// Declare Functions
+void scalarMultiply (int[][5], int); // Must declare second array dimension size
+void displayMatrix (int[][5]);
+
 int main(void) {
-  printf("Hello World\n");
+
+  // Declare and Define matrix
+  int sampleMatrix[3][5] = {
+  {  7, 16, 55, 13, 12 }, 
+  { 12, 10, 52,  0,  7 },
+  { -2,  1,  2,  4,  9 }
+  };
+
+  // Print original matrix
+  printf ("Original matrix:\n");
+  displayMatrix(sampleMatrix);
+
+  // Multiply matrix by 2
+  scalarMultiply (sampleMatrix, 2);
+
+  // Print new matrix 
+  printf ("\nMultiplied by 2:\n");
+  displayMatrix (sampleMatrix);
+
+  // Multiply matrix by -1
+  scalarMultiply (sampleMatrix, -1);
+
+  // Print new matrix
+  printf ("\nMultiplied by -1:\n");
+  displayMatrix (sampleMatrix);
+  
   return 0;
+}
+
+// Define Functions
+void scalarMultiply (int matrix [3][5], int scalar) {
+  for (int row = 0; row < 3; row++) {
+   for (int column = 0; column < 5; column++)
+     matrix[row][column] *= scalar;
+  }
+}
+
+void displayMatrix (int matrix[3][5]) {
+  for (int row = 0; row < 3; row++) {
+    for (int column = 0; column < 5; column++) {
+      printf ("%5i", matrix[row][column]);
+    }
+    printf ("\n");
+  }
+  printf ("\n");
 }
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Programming in C
+## Chapter 8
+### Question 1: Part 13
+
+This problem is from chapter 8 of the textbook[^1]. It is to copy program 8.13[^2] from the textbook. This program multiplies the elements of a two dimensional array by a scalar usinf a function.
+
+You may wish to run the program and compare its output.
+
+
+[^1]: Programming in C (Third edition) By Stephen G. Kochan - Page 162 - Question 1 - Part 13
+[^2]: Programming in C (Third edition) By Stephen G. Kochan - Page 147 - Program 8.13
